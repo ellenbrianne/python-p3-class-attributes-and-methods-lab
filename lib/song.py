25dd65f57cdd4648678ecfef1530714ebcb637e8
Ellen Bennett <bennette831@gmail.com>
2023-09-18 04:35:59 -0600

    create class attributes and class methods to update attributes on class instantiation

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,25 @@
 class Song:
-    pass
+
+    count = 0
+    genres = []
+    artists = []
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist 
+        self.genre = genre
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1 
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        cls.genres.append(genre) if genre not in cls.genres else None
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        cls.artists.append(artist) if artist not in cls.artists else None
