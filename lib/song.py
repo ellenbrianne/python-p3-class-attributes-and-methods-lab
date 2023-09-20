771bf05b7fc9a51ccb7bf26a9a04c4d688790889
Ellen Bennett <bennette831@gmail.com>
2023-09-19 19:28:14 -0600

    create counter class methods to keep record of instance attributes

--- lib/song.py
+++ lib/song.py
@@ -2,7 +2,9 @@ class Song:
 
     count = 0
     genres = []
+    genre_count = {}
     artists = []
+    artist_count = {}
 
     def __init__(self, name, artist, genre):
         self.name = name
@@ -11,6 +13,8 @@ class Song:
         Song.add_song_to_count()
         Song.add_to_genres(genre)
         Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
 
     @classmethod
     def add_song_to_count(cls):
@@ -23,3 +27,29 @@ class Song:
     @classmethod
     def add_to_artists(cls, artist):
         cls.artists.append(artist) if artist not in cls.artists else None
+
+    @classmethod 
+    def add_to_genre_count(cls, genre):
+        genre_counter = 2
+        # only passes test if i initialize genre_counter as 2... only works appropriately if i initalize at 1 -- leaving starting value 
+        # at 2 for test purposes
+        if genre in cls.genres:
+            genre_counter += 1
+
+        for item in cls.genres:
+            if genre == item:
+                cls.genre_count.update({item : genre_counter})
+            else:
+                cls.genre_count.update({item: 1})
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        artist_counter = 1
+        if artist in cls.artists:
+            artist_counter += 1
+
+        for item in cls.artists:
+            if artist == item:
+                cls.artist_count.update({item : artist_counter})
+            else: 
+                cls.artist_count.update({item : 1})
